44e8698060225abf51c543e1a5ae2305efdfc9bd
thomas <me@thomasanderson.cloud>
2020-06-15 00:28:59 -0400

    Handle local paths

--- app/assets/js/helpers.js
+++ app/assets/js/helpers.js
@@ -1,11 +1,14 @@
 Handlebars.registerHelper('cleanurl', function(url) {
-    var cleanurl = url.replace(/.*:\/\//,'').split(/[?#]/);
+    var currentpath = window.location.host + window.location.pathname;
+    var cleanurl = url.replace(/^\./, currentpath.substring(0, currentpath.lastIndexOf('/'))).replace(/.*:\/\//,'').split(/[?#]/);
     return new Handlebars.SafeString(cleanurl)
 });
 
 Handlebars.registerHelper('addprotocol', function(url) {
-    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
-        url = "https://" + url;
+    if (!/^\./.test(url)) {
+        if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+            url = "https://" + url;
+        }
     }
     return new Handlebars.SafeString(url)
 });
